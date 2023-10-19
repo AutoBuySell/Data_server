40a09886099b3f217fe27963b7cd99f7f39361b2
dkqp <ltg0513@gmail.com>
2023-10-19 13:31:36 +0900

    [#11]FEAT: historical data download endpoint

--- apps/dataarchiving.py
+++ apps/dataarchiving.py
@@ -2,10 +2,17 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi import Body
 
-from scripts.archiving import real_time_archiving
+from scripts.archiving import real_time_archiving, historical_archiving
 from apps.error import CustomError
 
 dataArchiving = FastAPI()
+
+@dataArchiving.exception_handler(CustomError)
+def custom_error_handler(request: Request, exc: CustomError):
+    return JSONResponse(
+        content={"message": f'{exc.message} in {exc.detail}'},
+        status_code=exc.status_code
+    )
 
 @dataArchiving.get('/')
 def hello_data_archiving():
@@ -36,9 +43,16 @@ def real_time(symbols: list = Body(embed=True)):
     status_code=201,
   )
 
-@dataArchiving.exception_handler(CustomError)
-def custom_error_handler(request: Request, exc: CustomError):
-    return JSONResponse(
-        content={"message": f'{exc.message} in {exc.detail}'},
-        status_code=exc.status_code
-    )
+@dataArchiving.post('/historical')
+def historical(data: dict):
+   symbol = data.get('symbol')
+   timeframe = data.get('timeframe')
+   startDate = data.get('startDate')
+   endDate = data.get('endDate')
+
+   historical_archiving(symbol, timeframe, startDate, endDate)
+
+   return JSONResponse(
+      content={"message": "success"},
+      status_code=201,
+   )
